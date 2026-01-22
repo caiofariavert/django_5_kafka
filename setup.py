457c254b0e2c1562b0e3019550dfcbeb1a6c9b65
Caio Faria <caio.faria@vert-capital.com>
2026-01-22 16:52:22 -0300

    fix: update package version to 2.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="django_kafka",
-    version="2.2.0",
+    version="2.2.1",
     author="VertCapital",
     author_email="thiago@vert-capital.com.br",
     description="Producer and Consumer for Kafka and django projects",
