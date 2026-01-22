ed432ae7c5f4f228b77210d211aca4187be7c205
Caio Faria <caio.faria@vert-capital.com>
2026-01-22 17:42:04 -0300

    remove bkp

--- bkp/django_kafka/management/commands/kafka_consumer.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from django_kafka.consumer import kafka_consumer_run
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options) -> None:
-        kafka_consumer_run()
